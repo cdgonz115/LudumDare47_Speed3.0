d781a2d74ccf1d7219ace4cb72388721143b87b8
Phyrol <nitehawkk123@gmail.com>
2020-10-03 17:15:52 -0700

    Powerups, traps, teleporter
    - Speed boost
    
    - Slow/Stop trap
    
    - teleporter back to starting point

---
+++ Assets/Scripts/Traps/StopTrap.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopTrap : MonoBehaviour
+{
+    private GameObject trapObject; //trap that will appear
+
+    private float trapTimeCounter; //actual timer
+    private float trapTime; //initial time for the counter
+
+    private void Start()
+    {
+        trapObject = transform.GetChild(0).gameObject;
+        trapTime = 1.0f;
+        trapTimeCounter = trapTime;
+    }
+
+    private void Update()
+    {
+        if(trapObject.activeSelf)
+        {
+            if(trapTimeCounter > 0)
+            {
+                trapTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                trapObject.SetActive(false);
+                trapTimeCounter = trapTime;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            trapObject.SetActive(true);
+        }
+    }
+}
