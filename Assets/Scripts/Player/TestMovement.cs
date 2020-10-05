c02c4cf35b2792feb2370fd6e7c2f5aa81f40d42
propredator23 <christiandash@hotmail.com>
2020-10-05 02:24:32 -0700

    GamePlay Teaks
    Added the functionality for walls to be destoryed after being hit when going at a certain speed, also improved on the traps and speedsters

--- Assets/Scripts/Player/TestMovement.cs
+++ Assets/Scripts/Player/TestMovement.cs
@@ -12,20 +12,22 @@ public class TestMovement : MonoBehaviour
     public float jumpHeight;
 
     public bool isGrounded;
+    public bool stop;
     public Transform groundCheck;
     public float groundeDistance;
 
     public CharacterController controller;
+    public float velocity;
     public Vector3 yVelocity;
     public Vector3 move;
     public Rigidbody rb;
 
-    public bool inBoost;
     public float boostMultiplier;
 
     public LayerMask mask;
 
     // Update is called once per frame
+
     private void Start()
     {
         speed = walkSpeed;
@@ -33,7 +35,7 @@ public class TestMovement : MonoBehaviour
     void Update()
     {
 
-        if (transform.position.y < 0) print(transform.position.y);
+        //if (transform.position.y < 0) print(transform.position.y);
         //if (transform.position.y < 0) transform.position = new Vector3(transform.position.x, 0, transform.position.z);
 
         isGrounded = Physics.CheckSphere(groundCheck.position,groundeDistance,mask);
@@ -43,8 +45,17 @@ public class TestMovement : MonoBehaviour
         }
         Move();
         Jump();
-
-        print(speed);
+    }
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.GetComponent<BreakAtSpeed>()) 
+        {
+            if (velocity >= hit.gameObject.GetComponent<BreakAtSpeed>().speedBreeak)
+            {
+                hit.gameObject.GetComponent<BreakAtSpeed>().Poof();
+            }
+            stop = true;
+        } 
     }
     void Move()
     {
@@ -58,20 +69,32 @@ public class TestMovement : MonoBehaviour
         else 
         {
             */
-        if (Input.GetKey(KeyCode.W)) z = (z >= speed) ? z + increaseRate : speed;
-        else if (Input.GetKey(KeyCode.S)) z = (z <= -speed) ? z - increaseRate : -speed;
-        else
+        if (stop)
         {
             z = 0;
+            x = 0;
+            velocity = 0;
+            stop = false;
             boostMultiplier = 1;
         }
-        if (Input.GetKey(KeyCode.D)) x = (x >= speed) ? x + increaseRate : speed;
-        else if (Input.GetKey(KeyCode.A)) x = (x <= -speed) ? x - increaseRate : -speed;
         else
         {
-            x = 0;
-            boostMultiplier = 1;
+            if (Input.GetKey(KeyCode.W)) z = (z >= speed) ? z + increaseRate : speed;
+            else if (Input.GetKey(KeyCode.S)) z = (z <= -speed) ? z - increaseRate : -speed;
+            else
+            {
+                z = 0;
+                boostMultiplier = 1;
+                velocity = 0;
+            }
+            if (Input.GetKey(KeyCode.D)) x = (x >= speed) ? x + increaseRate : speed;
+            else if (Input.GetKey(KeyCode.A)) x = (x <= -speed) ? x - increaseRate : -speed;
+            else
+            {
+                x = 0;
+            }
         }
+       
 
             //if (Input.GetKey(KeyCode.W)) rb.velocity = transform.forward * speed;
             //else if (Input.GetKey(KeyCode.S)) rb.velocity = transform.forward* -speed;
@@ -80,20 +103,17 @@ public class TestMovement : MonoBehaviour
 
 
             //}     
-            move = transform.right * x + transform.forward * z;
+        move = transform.right * x + transform.forward * z;
         controller.Move(move*Time.deltaTime * boostMultiplier);
+        velocity += Mathf.Abs( ( (transform.right* (x == 0 ? 0 : 1) + transform.forward* (z == 0 ? 0 : 1))* increaseRate).magnitude )  ;
         yVelocity.y += gravityRate * Time.deltaTime;
         //else yVelocity.y = 0;
         controller.Move(yVelocity * Time.deltaTime);
-        //print(rb.velocity);/
     }
+
     void Jump()
     {
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded) yVelocity.y = Mathf.Sqrt(jumpHeight * -2f * gravityRate);
-    }
-    void ApplyGravity()
-    { 
-        //if(isGrounded)
     }
     private void OnDrawGizmos()
     {
